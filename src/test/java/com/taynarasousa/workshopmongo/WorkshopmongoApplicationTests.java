d22debec4bdeb2fa962bb9f3d46336e1923274c8
Taynara Sousa <so.taynara@gmail.com>
2024-02-02 14:22:19 -0300

    Primeiro Commit - projeto criado

---
+++ src/test/java/com/taynarasousa/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taynarasousa.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
