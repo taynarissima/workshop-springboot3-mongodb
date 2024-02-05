68dc2eade2a81b782646e4fa1e36d6356996745a
Taynara Sousa <so.taynara@gmail.com>
2024-02-05 14:55:03 -0300

    User insertion with POST

--- src/main/java/com/taynarasousa/workshopmongo/services/UserService.java
+++ src/main/java/com/taynarasousa/workshopmongo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.taynarasousa.workshopmongo.services;
 
 import java.util.List;
-
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.taynarasousa.workshopmongo.domain.User;
+import com.taynarasousa.workshopmongo.dto.UserDTO;
 import com.taynarasousa.workshopmongo.repository.UserRepository;
 import com.taynarasousa.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
