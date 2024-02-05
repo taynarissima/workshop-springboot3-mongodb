fcae60939dc5e12bd0a963229ebaf158087fef52
Taynara Sousa <so.taynara@gmail.com>
2024-02-05 14:01:29 -0300

    Obtaining a user by id

---
+++ src/main/java/com/taynarasousa/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.taynarasousa.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
