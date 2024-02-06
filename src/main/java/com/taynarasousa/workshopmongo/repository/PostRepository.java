5f8e993f6a74f10396800e6b9a7a443135ffde64
Taynara Sousa <so.taynara@gmail.com>
2024-02-06 16:49:48 -0300

    Creating entity Post with nested User

---
+++ src/main/java/com/taynarasousa/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.taynarasousa.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.taynarasousa.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
